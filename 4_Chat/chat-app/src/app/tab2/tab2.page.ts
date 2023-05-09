f15658350db2320010a07f87f672e541b9097a2c
Andreas <andreas.morasch@hotmail.de>
2023-05-09 11:42:50 +0200

    Nachrichten werden angezeigt und können gesendet werden

--- 4_Chat/chat-app/src/app/tab2/tab2.page.ts
+++ 4_Chat/chat-app/src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Firestore, addDoc, collection, collectionData, orderBy, query, serverTimestamp } from '@angular/fire/firestore';
 import { Observable } from 'rxjs/internal/Observable';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -10,10 +11,12 @@ import { Observable } from 'rxjs/internal/Observable';
 export class Tab2Page {
 
   chats: Observable<any[]>;
+  username: string | undefined;
+  text: string | undefined;
 
   constructor(private firestore: Firestore) {
     const col = collection(firestore, 'room_0');
-    const q = query(col, orderBy('timestamp', 'desc'));
+    const q = query(col, orderBy('timestamp', 'asc'));
     this.chats = collectionData(q);
   }
 
@@ -26,12 +29,24 @@ export class Tab2Page {
     localStorage.setItem('username', JSON.stringify(username));
   }
 
-  async addMessage(author: string, text: string) {
+  async sendMessage() {
+    this.username = "Joe Doe" // TODO -> später gegen LocalStorage Zugriff ersetzen
+    if (this.username == undefined || this.text == undefined || this.text.length == 0) {
+      console.error("Eine Ihrer Daten ist unvollständig.");
+      return;
+    }
+
     await addDoc(collection(this.firestore, 'room_0'), {
-      'author': author,
-      'text': text,
+      'author': this.username,
+      'text': this.text,
       'timestamp': serverTimestamp()
-    })
+    });
+  }
+
+  onIonInfinite(ev: any) {
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
   }
 
 }
